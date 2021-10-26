f25bb47bd523790d455e4fa6921e09e87bb0e8b7
rafarth1 <rafarth1@outlook.com>
2021-10-26 10:17:42 -0500

    Cambios nuevos

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const countryClimate = document.querySelector(".country");
 const regionClimate = document.querySelector(".region");
 const nameCountryClimate = document.querySelector(".name");
 const iconImage = document.querySelector(".icon-climate");
+const loader = document.querySelector(".loader");
 
 const inputCountry = document.querySelector("#country");
 const btnSearchCountry = document.querySelector("#search-country").addEventListener("click", getInformationCountry);
@@ -12,37 +13,234 @@ document.addEventListener("DOMContentLoaded", () => {
 	inputCountry.addEventListener("input", readText);
 });
 
-let newInformationCountry = {
-	country: "",
-	KEY: "29b74c77ae35422f89c21401211410",
-};
+// Array list countries
+let countries = [
+	"Afghanistan",
+	"Albania",
+	"Algeria",
+	"Andorra",
+	"Angola",
+	"Antarctica",
+	"Antigua and Barbuda",
+	"Argentina",
+	"Armenia",
+	"Australia",
+	"Austria",
+	"Azerbaijan",
+	"Bahamas",
+	"Bahrain",
+	"Bangladesh",
+	"Barbados",
+	"Belarus",
+	"Belgium",
+	"Belize",
+	"Benin",
+	"Bermuda",
+	"Bhutan",
+	"Bolivia",
+	"Bosnia and Herzegovina",
+	"Botswana",
+	"Brazil",
+	"Brunei",
+	"Bulgaria",
+	"Burkina Faso",
+	"Burma",
+	"Burundi",
+	"Cambodia",
+	"Cameroon",
+	"Canada",
+	"Cape Verde",
+	"Central African Republic",
+	"Chad",
+	"Chile",
+	"China",
+	"Colombia",
+	"Comoros",
+	"Congo, Democratic Republic",
+	"Congo, Republic of the",
+	"Costa Rica",
+	"Cote d'Ivoire",
+	"Croatia",
+	"Cuba",
+	"Cyprus",
+	"Czech Republic",
+	"Denmark",
+	"Djibouti",
+	"Dominica",
+	"Dominican Republic",
+	"East Timor",
+	"Ecuador",
+	"Egypt",
+	"El Salvador",
+	"Equatorial Guinea",
+	"Eritrea",
+	"Estonia",
+	"Ethiopia",
+	"Fiji",
+	"Finland",
+	"France",
+	"Gabon",
+	"Gambia",
+	"Georgia",
+	"Germany",
+	"Ghana",
+	"Greece",
+	"Greenland",
+	"Grenada",
+	"Guatemala",
+	"Guinea",
+	"Guinea-Bissau",
+	"Guyana",
+	"Haiti",
+	"Honduras",
+	"Hong Kong",
+	"Hungary",
+	"Iceland",
+	"India",
+	"Indonesia",
+	"Iran",
+	"Iraq",
+	"Ireland",
+	"Israel",
+	"Italy",
+	"Jamaica",
+	"Japan",
+	"Jordan",
+	"Kazakhstan",
+	"Kenya",
+	"Kiribati",
+	"Korea, North",
+	"Korea, South",
+	"Kuwait",
+	"Kyrgyzstan",
+	"Laos",
+	"Latvia",
+	"Lebanon",
+	"Lesotho",
+	"Liberia",
+	"Libya",
+	"Liechtenstein",
+	"Lithuania",
+	"Luxembourg",
+	"Macedonia",
+	"Madagascar",
+	"Malawi",
+	"Malaysia",
+	"Maldives",
+	"Mali",
+	"Malta",
+	"Marshall Islands",
+	"Mauritania",
+	"Mauritius",
+	"Mexico",
+	"Micronesia",
+	"Moldova",
+	"Mongolia",
+	"Morocco",
+	"Monaco",
+	"Mozambique",
+	"Namibia",
+	"Nauru",
+	"Nepal",
+	"Netherlands",
+	"New Zealand",
+	"Nicaragua",
+	"Niger",
+	"Nigeria",
+	"Norway",
+	"Oman",
+	"Pakistan",
+	"Panama",
+	"Papua New Guinea",
+	"Paraguay",
+	"Peru",
+	"Philippines",
+	"Poland",
+	"Portugal",
+	"Qatar",
+	"Romania",
+	"Russia",
+	"Rwanda",
+	"Samoa",
+	"San Marino",
+	" Sao Tome",
+	"Saudi Arabia",
+	"Senegal",
+	"Serbia and Montenegro",
+	"Seychelles",
+	"Sierra Leone",
+	"Singapore",
+	"Slovakia",
+	"Slovenia",
+	"Solomon Islands",
+	"Somalia",
+	"South Africa",
+	"Spain",
+	"Sri Lanka",
+	"Sudan",
+	"Suriname",
+	"Swaziland",
+	"Sweden",
+	"Switzerland",
+	"Syria",
+	"Taiwan",
+	"Tajikistan",
+	"Tanzania",
+	"Thailand",
+	"Togo",
+	"Tonga",
+	"Trinidad and Tobago",
+	"Tunisia",
+	"Turkey",
+	"Turkmenistan",
+	"Uganda",
+	"Ukraine",
+	"United Arab Emirates",
+	"United Kingdom",
+	"United States",
+	"Uruguay",
+	"Uzbekistan",
+	"Vanuatu",
+	"Venezuela",
+	"Vietnam",
+	"Yemen",
+	"Zambia",
+	"Zimbabwe",
+];
+
+// Functions
 
 function readText(e) {
 	newInformationCountry[e.target.id] = e.target.value;
 }
 
-// Petition with AXIOS the API
-function petitionAPI() {
+let newInformationCountry = {
+	country: "",
+	KEY: "29b74c77ae35422f89c21401211410",
+};
+
+// Mejorar algunas cosas y agregar algunas mas funcionalidad
+async function getInformationCountry() {
 	let KEY = "29b74c77ae35422f89c21401211410";
 
-	axios({
-		method: "get",
-		baseURL: "http://api.weatherapi.com/v1",
-		url: `/current.json?key=${KEY}&q=${newInformationCountry.country}`,
-	}).then((res) => {
-		puttingInformactionAPI(res);
-	});
-}
+	loader.style.display = "inline-block";
 
-function getInformationCountry() {
-	if (inputCountry.value) {
-		petitionAPI();
+	if (countries.includes(newInformationCountry.country)) {
+		const response = await axios({
+			method: "get",
+			baseURL: "http://api.weatherapi.com/v1",
+			url: `/current.json?key=${KEY}&q=${newInformationCountry.country}&alerts=yes`,
+		});
+		console.log(response);
+		printInformation(response); // @Sending all HTML
 	} else {
-		message("Falta Rellenar");
+		message("Falta Rellenar o ingresaste país equivocado");
 	}
+
+	loader.style.display = "none";
 }
 
-function puttingInformactionAPI(res) {
+function printInformation(res) {
 	// Creando la imagen
 	removeChild(iconImage);
 
@@ -52,8 +250,6 @@ function puttingInformactionAPI(res) {
 		},
 		location: { country, region, name },
 	} = res.data;
-
-	console.log(text);
 
 	const imageClimate = document.createElement("img");
 	imageClimate.src = icon;
@@ -88,7 +284,7 @@ function message(typeMessage) {
 
 	setTimeout(() => {
 		message.remove();
-	}, 1000);
+	}, 2000);
 }
 
 function removeChild(elementParent) {
@@ -103,82 +299,3 @@ function removeChild(elementParent) {
 		elementParent.parentNode.removeChild(elementParent);
 	}
 }
-
-/* function getInformationCountry() {
-	removeChild(iconImage);
-
-	const inputCountry = document.querySelector("#country");
-
-	informationCountry = {
-		country: inputCountry,
-	};
-
-	// Petition with AXIOS the API
-	if (inputCountry) {
-		let KEY = "29b74c77ae35422f89c21401211410";
-
-		axios({
-			method: "get",
-			baseURL: "http://api.weatherapi.com/v1",
-			url: `/current.json?key=${KEY}&q=${informationCountry.country}`,
-		}).then((res) => {
-			// Creando la imagen
-			const imageClimate = document.createElement("img");
-			imageClimate.src = res.data.current.condition.icon;
-			imageClimate.alt = "Image Climate";
-
-			// the countries informations
-			country.textContent = res.data.location.country;
-			region.textContent = res.data.location.region;
-			nameCountryClimate.textContent = res.data.location.name;
-
-			//  DOM
-			iconImage.appendChild(imageClimate);
-		});
-	} else {
-		console.log("Rellenar Campo");
-
-		errorMessage();
-	}
-}
-
-function errorMessage() {
-	const inputCountry = document.querySelector("#country").addEventListener("input", () => {
-		console.log(e.target.value);
-	});
-	const contentError = document.querySelector(".content-error");
-
-	// Creando el mensaje
-	const messagError = document.createElement("p");
-	const message = document.createTextNode("Rellanar campo obligatorio");
-
-	messagError.appendChild(message);
-	contentError.appendChild(messagError);
-
-	// Insertanto estilos para el mensaje de error
-
-	messagError.classList.add("errorMessage");
-}
-
-// IMPORTANTE ENTENDER ESTO FALTA ENTEDERLO
-
-function removeChild(elementParent) {
-	while (elementParent.hasChildNodes()) {
-		clear(elementParent.firstChild);
-	}
-
-	function clear(elementParent) {
-		while (elementParent.hasChildNodes()) {
-			clear(elementParent.firstChild);
-		}
-		elementParent.parentNode.removeChild(elementParent);
-	}
-}
-
-const inputCountry2 = document.querySelector("#country2");
-inputCountry2.addEventListener("input", function (e) {
-	console.log(e.target.value);
-});
-
-console.log(inputCountry2);
- */
